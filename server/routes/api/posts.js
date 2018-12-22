f9a5d956d99bbf8ef047f0aa72a6c2b6e8e1b7ab
Krisz Magyar <krisz.magyar@gmail.com>
2018-12-22 23:47:49 +0100

    init server

---
+++ server/routes/api/posts.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection()
+    res.send(await posts.find({}).toArray())
+})
+
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection()
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    })
+    res.status(201).send()
+})
+
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection()
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send()
+})
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect(
+        'mongodb://asd123:asd123@ds141674.mlab.com:41674/vue-express',
+        { useNewUrlParser: true }
+    )
+    return client.db('vue-express').collection('posts')
+}
+
+module.exports = router
