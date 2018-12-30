46efe7ba7e9dc6087b667f3a42a96f203a147683
Krisz Magyar <krisz.magyar@gmail.com>
2018-12-30 13:07:38 +0100

    fix dirname

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use('/api/posts', posts)
 
 if(process.env.NODE_ENV === 'production') {
     // Static folder
-    app.use(express.static(___dirname + '/public'))
+    app.use(express.static(__dirname + '/public'))
 
     // Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
