611f3b82757330febed56dd2ef874dafbd6ee919
Krisz Magyar <krisz.magyar@gmail.com>
2018-12-30 11:25:23 +0100

    add create and delete post funcionality

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,32 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="container">
+    <h1>Latest Posts</h1>
+    <div class="create-post">
+      <label for="create-post">Say something...</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Create a post">
+      <button v-on:click="createPost">Post</button>
+    </div>
+    <hr>
+    <p class="error" v-if="error">{{ error }}</p>
+    <div class="posts-container">
+      <div class="post"
+        v-for="post in posts"
+        v-bind:key="post._id"
+        v-on:dblclick="deletePost(post._id)"
+      >
+        {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}` }}
+        <p class="text">{{ post.text }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import PostService from '../PostService'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'PostComponent',
+  data() {
+    return {
+      posts: [],
+      error: '',
+      text: ''
+    }
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts()
+    } catch (err) {
+      this.error = err.message
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text)
+      this.posts = await PostService.getPosts()
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id)
+      this.posts = await PostService.getPosts()
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
